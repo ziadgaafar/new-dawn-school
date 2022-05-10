e5b1435e3099ff50f2931d52956e46a1a20d69e8
ziadgaafar <ziadgaafar@protonmail.com>
2022-05-10 20:32:36 +0200

    initial commit

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
+app.listen(3001, () => {
+  console.log("server is running on port 3001");
+});
